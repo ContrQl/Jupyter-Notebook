3d8fefa85953941a41884ce03c477c6f51a9c926
Mujie <mujiechen@Levis-MacBook-Pro.local>
2024-11-22 19:52:31 +0000

    no change

--- R (for Meta-analyses)/Meta-analysis.r
+++ R (for Meta-analyses)/Meta-analysis.r
@@ -1,5 +1,14 @@
-m.cont <- metacont(n.e = n1, mean.e = m1, sd.e = s1,
-n.c = n2, mean.c = m2, sd.c = s2,
-studlab = Study, data = “Metadata”, sm = "SMD", 
-method.smd = "Hedges", common = FALSE, 
-random = TRUE, title = ”SSRI versus placebo for glycaemia")
+# Make sure XQuartz is running
+
+library(readxl)
+Metadata <- read_excel("/Users/mujiechen/Jupyter-Notebook/R/Datasets/Metadata.xlsx")
+View(Metadata)
+
+library("meta")
+
+m.cont <- metacont(n.e = Metadata$n1, mean.e = Metadata$m1, sd.e = Metadata$s1, n.c = Metadata$n2, mean.c = Metadata$m2, sd.c = Metadata$s2, studlab = Metadata$Study, data = Metadata, sm = "SMD", method.smd = "Hedges", common = FALSE, random = TRUE, title = ("SSRI versus placebo"))
+
+summary(m.cont)
+
+# Make notes for everything when revisiting this lecture
+# Possibly shift to Jupyter Notebook?
